11a3726f97f509c764ebb2d6c4ae0b130f4e8023
zuyfus25 <onphu2k5@gmail.com>
2026-01-03 17:35:58 +0700

    comment + like + upload image

--- src/models/post-reply.model.ts
+++ src/models/post-reply.model.ts
@@ -1,21 +1,22 @@
 import { db } from "../configs/db";
 
 export interface IPostReply {
-    id: string,
+    id?: string,
     user_id: string,
     post_id: string,
     content: string,
-    created_at: Date,
-    updated_at: Date
+    created_at?: Date,
+    updated_at?: Date
 }
 
 class PostReplyModel {
     async findByPostId(post_id: string): Promise<IPostReply[]> {
         const sql = `
-            SELECT *
-            FROM post_replies
-            WHERE post_id = $1
-            ORDER BY created_at DESC
+            SELECT r.*, u.full_name, u.avatar_url
+            FROM post_reply r
+            LEFT JOIN users u ON r.user_id = u.id
+            WHERE r.post_id = $1
+            ORDER BY r.created_at DESC
         `;
         const result = await db.query(sql, [post_id]);
         return result.rows;
@@ -24,7 +25,7 @@ class PostReplyModel {
     async findById(id: string): Promise<IPostReply | null> {
         const sql = `
             SELECT *
-            FROM post_replies
+            FROM post_reply
             WHERE id = $1
         `;
         const result = await db.query(sql, [id]);
@@ -33,7 +34,7 @@ class PostReplyModel {
 
     async create(reply: IPostReply) {
         const sql = `
-            INSERT INTO post_replies (user_id, post_id, content, created_at, updated_at)
+            INSERT INTO post_reply (user_id, post_id, content, created_at, updated_at)
             VALUES ($1, $2, $3, NOW(), NOW())
             RETURNING *
         `;
@@ -42,12 +43,13 @@ class PostReplyModel {
             reply.post_id,
             reply.content
         ]);
+        await db.query(`UPDATE posts SET reply_count = reply_count + 1 WHERE id = $1`, [reply.post_id]);
         return result.rows[0];
     }
 
     async update(id: string, payload: Partial<IPostReply>) {
         const sql = `
-            UPDATE post_replies
+            UPDATE post_reply
             SET content = $1, updated_at = NOW()
             WHERE id = $2
             RETURNING *
@@ -58,7 +60,7 @@ class PostReplyModel {
 
     async delete(id: string) {
         const sql = `
-            DELETE FROM post_replies
+            DELETE FROM post_reply
             WHERE id = $1
         `;
         await db.query(sql, [id]);
