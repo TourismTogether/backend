5aa89231f261a8642a4b7100129364a8baaffc5f
hlam3004 <hlam.30042005@gmail.com>
2026-01-04 21:09:55 +0700

    change config session

--- src/server.ts
+++ src/server.ts
@@ -78,8 +78,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day 
     },
   })
